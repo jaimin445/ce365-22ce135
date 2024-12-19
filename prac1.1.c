1e3941a0f06ee3be049e0bc711287e21e61da58a
jaimin445 <133215320+jaimin445@users.noreply.github.com>
2024-12-19 10:58:08 +0530

    Update prac1.1.c

--- prac1.1.c
+++ prac1.1.c
@@ -1,39 +1,42 @@
-// this program checks a*bb
+// this program checks a*bb 
+
 #include<stdio.h>
-// #include<conio.h>
 
-int main(){
-    char str[200];
-    int flag=0,flag2=0,count=0;
-    gets(str);
+int main() {
+    char str[200]; // character array to store the input string
+    int flag = 0, flag2 = 0, count = 0; // Initializes flags and counter
+    gets(str); // Read input string
 
-    for(int i=0;str[i]!=NULL;i++){
-        if(str[i]=='a'||str[i]=='b'){
-            if(str[i]=='a'){
-                if(flag==1){
+    // loop run until its found null 
+    for(int i = 0; str[i] != NULL; i++) {
+    
+        if (str[i] == 'a' || str[i] == 'b') { //only a and b is allow otherwise invalid
+            if (str[i] == 'a') { 
+            
+                if (flag == 1) { // its check a shouldnt encounter after b
+                    printf("invalid string"); //invalid string message
+                    flag2 = 1; // error flag
+                    break; 
+                }
+            } 
+            else if (str[i] == 'b') { 
+                count++; 
+                flag = 1; //  this  flag indicating 'b' has been encountered
+                if (count > 2) {  // more than two 'b's are not allowed otherwise the string is invalid
                     printf("invalid string");
-                    flag2=1;
-                    break;
+                    flag2 = 1; // Sets the error flag
+                    break; 
                 }
             }
-            else if(str[i]=='b'){
-                count++;
-                flag=1;
-                if(count>2){
-                    printf("invalid string");
-                    flag2=1;
-                    break;
-                }
-            }
-        }
-        else{
-            printf("invalid string");
-            flag2=1;
-            break;
+        } 
+        else {
+            printf("invalid string"); 
+            flag2 = 1; 
+            break; 
         }
     }
 
-    if(flag2!=1){
-        printf("string is valid");
+    if (flag2 != 1) {
+        printf("string is valid"); 
     }
 }
